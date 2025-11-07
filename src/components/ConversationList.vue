cdcd5dc483a47108ddd2b1cd1602084dd58a8dcc
chenting <3213043406@qq.com>
2025-11-07 21:39:17 +0800

    信息创建第二部分

--- src/components/ConversationList.vue
+++ src/components/ConversationList.vue
@@ -10,7 +10,7 @@
           class="flex justify-between items-center text-sm leading-5 text-gray-500"
         >
           <span>{{ item.selectedModel }}</span>
-          <span>{{ item.updatedAt }}</span>
+          <span>{{ dayjs(item.updatedAt).format("YYYY-MM-DD") }}</span>
         </div>
         <h2 class="font-semibold leading-6 text-gray-900 truncate">
           {{ item.title }}
@@ -22,6 +22,7 @@
 
 <script lang="ts" setup>
 import { useRouter } from "vue-router";
+import dayjs from "dayjs";
 import { ConversationProps } from "../types";
 defineProps<{ items: ConversationProps[] }>();
 const router = useRouter();
