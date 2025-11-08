225c46f6d06199bbae60b80336964c5f7450ecc0
chenting <3213043406@qq.com>
2025-11-08 15:19:29 +0800

    学习Pinia 了解getters/actions

--- src/stores/conversation.ts
+++ src/stores/conversation.ts
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { db } from "../db";
 import { ConversationProps } from "../types";
 
 export interface ConversationStore {
@@ -10,5 +11,25 @@ export const useConversationStore = defineStore("conversation", {
     return {
       items: [],
     };
+  },
+  actions: {
+    async fetchConversations() {
+      const items = await db.conversations.toArray();
+      this.items = items;
+    },
+    async createConversation(createdData: Omit<ConversationProps, "id">) {
+      const newCId = await db.conversations.add(createdData);
+      this.items.push({
+        id: newCId,
+        ...createdData,
+      });
+      return newCId;
+    },
+  },
+  getters: {
+    totalNumber: (state) => state.items.length,
+    getConversationById: (state) => (id: number) => {
+      return state.items.find((item) => item.id === id);
+    },
   },
 });
