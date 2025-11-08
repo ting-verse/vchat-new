9ae676d30799e74668f6afbb57aee54c2980d316
chenting <3213043406@qq.com>
2025-11-08 16:25:49 +0800

    实现上下文继续聊天

--- src/views/Conversation.vue
+++ src/views/Conversation.vue
@@ -10,7 +10,7 @@
     <MessageList :messages="filteredMessages" />
   </div>
   <div class="w-[80%] mx-auto h-[15%] flex items-center">
-    <MessageInput />
+    <MessageInput @create="sendNewMessage" v-model="inputValue" />
   </div>
 </template>
 <script lang="ts" setup>
@@ -22,11 +22,21 @@ import { useConversationStore } from "../stores/conversation";
 import { useMessageStore } from "../stores/message";
 import { MessageProps, ConversationProps, MessageStatus } from "../types";
 import { db } from "../db";
+const inputValue = ref("");
 const route = useRoute();
 const conversationStore = useConversationStore();
 const messageStore = useMessageStore();
 const filteredMessages = computed(() => messageStore.items);
-
+const sendedMessages = computed(() =>
+  filteredMessages.value
+    .filter((message) => message.status !== "loading")
+    .map((message) => {
+      return {
+        role: message.type === "question" ? "user" : "assistant",
+        content: message.content,
+      };
+    })
+);
 let conversationId = ref(parseInt(route.params.id as string));
 const initMessageId = parseInt(route.query.init as string);
 const convsersation = computed(() =>
@@ -35,6 +45,20 @@ const convsersation = computed(() =>
 const lastQuestion = computed(() =>
   messageStore.getLastQuestion(conversationId.value)
 );
+const sendNewMessage = async (question: string) => {
+  if (question) {
+    const date = new Date().toISOString();
+    await messageStore.createMessage({
+      content: question,
+      conversationId: conversationId.value,
+      createdAt: date,
+      updatedAt: date,
+      type: "question",
+    });
+    inputValue.value = "";
+    creatingInitialMessage();
+  }
+};
 const creatingInitialMessage = async () => {
   const createdData: Omit<MessageProps, "id"> = {
     content: "",
@@ -55,7 +79,7 @@ const creatingInitialMessage = async () => {
         messageId: newMessageId,
         providerName: provider.name,
         selectedModel: convsersation.value.selectedModel,
-        content: lastQuestion.value?.content || "",
+        messages: sendedMessages.value,
       });
     }
   }
